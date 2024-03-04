94569940d1c2fbbe8229920a9caab7418d952d3f
chris <125780008+Ascii-Hermit@users.noreply.github.com>
2024-03-04 12:21:33 +0530

    Create fail.py

---
+++ Week_8/fail.py
@@ -0,0 +1,76 @@
+#the probability that the numbers will just randomly arrange themselves is very low (9!) so this method is very useless
+
+import random
+
+def get_value(node):
+	sum_val = 0
+	addend_val1 = 0
+	addend_val2 = 0
+	
+	for i in node[2]:
+		sum_val = 10*sum_val + elements[i]
+	
+	for i in node[0]:
+		addend_val1 = 10*addend_val1 + elements[i]
+
+	for i in node[1]:
+		addend_val2 = 10*addend_val2 + elements[i] 
+
+	return abs(sum_val - (addend_val1+addend_val2))
+
+def iterate(elements,add1,add2,add_sum):
+
+	i = random.choice(list(elements.items()))[0]
+	j = random.choice(list(elements.items()))[0]
+	curr_node = [add1,add2,add_sum,elements]
+	curr_val = get_value(curr_node)
+	while True:
+		ele = curr_node[3]
+	
+		temp = elements[i]
+		elements[i] = elements[j]
+		elements[j] = temp
+
+		node = [add1,add2,add_sum,elements]
+		node_val = get_value(node)
+		
+    
+
+		if node_val<curr_val:
+			print(node_val)
+			curr_node = node
+			curr_val = node_val
+			ele = node[3]
+
+		if node_val == 0:
+			print("done")
+			return ele
+
+
+		i = random.choice(list(elements.items()))[0]
+		j = random.choice(list(elements.items()))[0]
+		
+		
+
+
+add1 = 'SEND' #input("Enter 1st string")
+add2 = 'MORE' #input("Enter 2nd string")
+add_sum = 'MONEY' #input("Enter the sum of the numbers")
+
+elements = {}
+numbers = [0,1,2,3,4,5,6,7,8,9]
+for i in add1:
+	if i not in elements.keys():
+		elements[i] = numbers.pop(0)
+		
+for i in add2:
+		if i not in elements.keys():
+			elements[i] = numbers.pop(0)
+
+for i in add_sum:
+	if i not in elements.keys():
+		elements[i] = numbers.pop(0)
+
+print(elements)
+elements = iterate(elements,add1,add2,add_sum)
+print(elements)
